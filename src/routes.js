540d0ed7203e5e1424f5d7d4004ed7b01c55f1c4
Tábata Baesso <tatabaesso@gmail.com>
2020-04-26 20:51:59 -0300

    creating addresses post and relating to users

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 const express = require('express');
+
 const UserController = require('./controllers/UserController');
+const AddressController = require('./controllers/AddressController');
 
 const routes = express.Router();
 
 routes.get('/users', UserController.index);
 routes.post('/users', UserController.store);
+
+routes.post('/users/:user_id/addresses', AddressController.store);
 
 module.exports = routes;
