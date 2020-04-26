b69681e212340681c90ef7375ef4c82e6f657b93
Tábata Baesso <tatabaesso@gmail.com>
2020-04-26 20:18:58 -0300

    get users with api

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const UserController = require('./controllers/UserController');
 
 const routes = express.Router();
 
+routes.get('/users', UserController.index);
 routes.post('/users', UserController.store);
 
 module.exports = routes;
