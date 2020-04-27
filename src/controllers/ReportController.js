8feb5b2acaa838dabdaf31f79183cb6ea60dcac9
Tábata Baesso <tatabaesso@gmail.com>
2020-04-26 22:50:13 -0300

    describing complex query

--- src/controllers/ReportController.js
+++ src/controllers/ReportController.js
@@ -3,6 +3,10 @@ const User = require('../models/User');
 
 module.exports = {
     async show(req, res) {
+        // Encontrar todos usuários que tem email que termina com "gmail.com"
+        // Desses usuários eu quero buscar todos que moram na rua "Rua legal da capital"
+        // Desses usuários eu quero buscar as tecnologia que começam com React
+
         const users = await User.findAll({
             attributes: ['name', 'email'],
             where: {
