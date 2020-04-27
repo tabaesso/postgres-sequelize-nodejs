6c7c370b1ca619d6d6dac96a4a7eefbaec0a3de6
Tábata Baesso <tatabaesso@gmail.com>
2020-04-26 22:22:58 -0300

    remove tech of user

--- src/controllers/TechController.js
+++ src/controllers/TechController.js
@@ -23,5 +23,24 @@ module.exports = {
         await user.addTech(tech);
 
         return res.json(tech);
+    },
+
+    async delete(req, res) {
+        const { user_id } = req.params;
+        const { name } = req.body;
+
+        const user = await User.findByPk(user_id);
+
+        if (!user) {
+            return res.status(400).json({ error: 'User not found' });
+        }
+
+        const tech = await Tech.findOne({
+            where: { name }
+        });
+
+        await user.removeTech(tech);
+
+        return res.json();
     }
 };
--- src/routes.js
+++ src/routes.js
@@ -14,5 +14,6 @@ routes.post('/users/:user_id/addresses', AddressController.store);
 
 routes.get('/users/:user_id/techs', TechController.index);
 routes.post('/users/:user_id/techs', TechController.store);
+routes.delete('/users/:user_id/techs', TechController.delete);
 
 module.exports = routes;
