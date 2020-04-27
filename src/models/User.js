48266f10c7c9bafe1082ecf77731e046e635b8c0
Tábata Baesso <tatabaesso@gmail.com>
2020-04-26 22:17:17 -0300

    create n:n relation with techs

--- src/models/User.js
+++ src/models/User.js
@@ -11,7 +11,8 @@ class User extends Model {
     }
 
     static associate(models) {
-        this.hasMany(models.Address, { foreignKey: 'user_id', as: 'addresses' })
+        this.hasMany(models.Address, { foreignKey: 'user_id', as: 'addresses' });
+        this.belongsToMany(models.Tech, { foreignKey: 'user_id', through: 'user_techs', as: 'techs' });
     }
 }
 
