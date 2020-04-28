b628657d54b9dd26b18854f6bf7c181e5f222b11
Tábata Baesso <tatabaesso@gmail.com>
2020-04-28 01:36:34 -0300

    alterando port para 3333

--- src/server.js
+++ src/server.js
@@ -8,6 +8,6 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.listen(3000, () => console.log('ta funcionando'));
+app.listen(3333, () => console.log('ta funcionando'));
 
 // app.listen(3000);
